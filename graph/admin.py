0ee5a0c44993b8364b6792db609a7385e6b54033
cordrey <mcordrey@gmail.com>
2016-09-09 11:23:28 -0500

    Changing admin default time display to '"Y-m-d H:i" and editing model TJROS to return str

--- graph/admin.py
+++ graph/admin.py
@@ -1,6 +1,10 @@
 
 from django.contrib import admin
 from .models import Tjros
+from django.conf.locale.en import formats as en_formats
 
+en_formats.DATETIME_FORMAT = "Y-m-d H:i"
+
+#TODO: add search
 
 admin.site.register(Tjros)
